e6a695391b53de6faa9dc2d285ff61f0fe57c474
Aditya Makhija <105123484+adityamakhija03@users.noreply.github.com>
2024-05-24 01:36:36 +0530

    commit

--- backend/src/controllers/payment.controller.js
+++ backend/src/controllers/payment.controller.js
@@ -1,8 +1,8 @@
 // const jwt = require('jsonwebtoken');
-const { kv } = require('@vercel/kv'); // Import the appropriate Redis client library
+import jwt from 'jsonwebtoken';
+import { kv } from '@vercel/kv';  // Import the appropriate Redis client library
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); // Import Stripe library
 import { asyncHandler } from '../utils/asyncHandler.js';
-import jwt from "jsonwebtoken";
 const createOrderSession = asyncHandler(async (req, res) => {
     try {
         const userId = req.user;
